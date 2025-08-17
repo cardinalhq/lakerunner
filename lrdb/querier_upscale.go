89d9b541e94dd2daf149fae08bc125cda28bf88e
Aditya <aditya@cardinalhq.io>
2025-08-17 11:57:59 -0700

    [ingest_traces] finish producer - write to s3, trace_seg and work_queue
    Signed-off-by: Aditya <aditya@cardinalhq.io>

--- lrdb/querier_upscale.go
+++ lrdb/querier_upscale.go
@@ -27,6 +27,10 @@ type MetricSegmentInserter interface {
 	InsertMetricSegment(ctx context.Context, params InsertMetricSegmentParams) error
 }
 
+type TraceSegmentInserter interface {
+	InsertTraceSegment(ctx context.Context, params InsertTraceSegmentDirectParams) error
+}
+
 type QuerierFull interface {
 	Querier
 	ReplaceMetricSegs(ctx context.Context, args ReplaceMetricSegsParams) error
@@ -45,5 +49,6 @@ type StoreFull interface {
 	QuerierFull
 	LogSegmentUpserter
 	MetricSegmentInserter
+	TraceSegmentInserter
 	WorkQueueQuerier
 }
