2fea92f849732f78eff3815d2ac3927cbc074a20
Michael Graff <mgraff@cardinalhq.io>
2025-08-30 18:28:05 -0500

    admin metric estimates summary

---
+++ cmd/admin/metrics/estimates.go
@@ -0,0 +1,32 @@
+// Copyright (C) 2025 CardinalHQ, Inc
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+package metrics
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/cardinalhq/lakerunner/cmd/admin/metrics/estimates"
+)
+
+func GetEstimatesCmd() *cobra.Command {
+	estimatesCmd := &cobra.Command{
+		Use:   "estimates",
+		Short: "Metric estimates administrative commands",
+	}
+
+	estimatesCmd.AddCommand(estimates.GetSummaryCmd())
+
+	return estimatesCmd
+}
