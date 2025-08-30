f35c3ae1d1a1da40d16234fe73af8b065b9f3ecf
Michael Graff <mgraff@cardinalhq.io>
2025-08-30 15:38:16 -0500

    split into smaller files

--- internal/metricsprocessing/compaction/run_loop.go
+++ internal/metricsprocessing/compaction/run_loop.go
@@ -20,35 +20,8 @@ import (
 	"time"
 
 	"github.com/cardinalhq/lakerunner/internal/awsclient"
-	"github.com/cardinalhq/lakerunner/internal/heartbeat"
 	"github.com/cardinalhq/lakerunner/internal/storageprofile"
-	"github.com/cardinalhq/lakerunner/lrdb"
 )
-
-// mcqHeartbeatStore defines the minimal interface needed for MCQ heartbeat operations
-type mcqHeartbeatStore interface {
-	TouchMetricCompactionWork(ctx context.Context, params lrdb.TouchMetricCompactionWorkParams) error
-}
-
-// newMCQHeartbeater creates a new heartbeater for the given claimed MCQ items
-func newMCQHeartbeater(db mcqHeartbeatStore, workerID int64, items []int64) *heartbeat.Heartbeater {
-	if len(items) == 0 {
-		// Return a no-op heartbeater for empty items
-		return heartbeat.New(func(ctx context.Context) error {
-			return nil // No-op
-		}, time.Minute, slog.Default().With("component", "mcq_heartbeater", "worker_id", workerID, "item_count", 0))
-	}
-
-	heartbeatFunc := func(ctx context.Context) error {
-		return db.TouchMetricCompactionWork(ctx, lrdb.TouchMetricCompactionWorkParams{
-			Ids:       items,
-			ClaimedBy: workerID,
-		})
-	}
-
-	logger := slog.Default().With("component", "mcq_heartbeater", "worker_id", workerID, "item_count", len(items))
-	return heartbeat.New(heartbeatFunc, time.Minute, logger)
-}
 
 func runLoop(
 	ctx context.Context,
