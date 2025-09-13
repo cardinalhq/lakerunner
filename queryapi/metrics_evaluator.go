cd571b8aab2b231198d636d797b0eefc6d65a819
ruchirj <ruchir.jha@gmail.com>
2025-09-12 17:06:21 -0700

    MergeSort fixes

--- queryapi/metrics_evaluator.go
+++ queryapi/metrics_evaluator.go
@@ -1,7 +1,7 @@
 // Copyright (C) 2025 CardinalHQ, Inc
 //
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License, as
+// it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, version 3.
 //
 // This program is distributed in the hope that it will be useful,
