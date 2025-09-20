bb07a4af03bf7dd8ad0e75335a58aa2f98565b87
Michael Graff <mgraff@cardinalhq.io>
2025-09-20 18:53:01 -0500

    add _cardinalhq.value=1 back to logs we write

--- internal/metricsprocessing/logs_ingest_processor.go
+++ internal/metricsprocessing/logs_ingest_processor.go
@@ -638,6 +638,7 @@ func (t *LogTranslator) TranslateRow(row *filereader.Row) error {
 	// Ensure required CardinalhQ fields are set
 	(*row)[wkk.RowKeyCTelemetryType] = "logs"
 	(*row)[wkk.RowKeyCName] = "log.events"
+	(*row)[wkk.RowKeyCValue] = int64(1)
 
 	return nil
 }
