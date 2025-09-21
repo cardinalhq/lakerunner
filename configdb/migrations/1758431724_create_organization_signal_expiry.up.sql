d3e2329fea9bdde535d4fd89e164776791682a26
Michael Graff <mgraff@cardinalhq.io>
2025-09-21 17:17:43 -0500

    remove extra copyright/license blob

--- configdb/migrations/1758431724_create_organization_signal_expiry.up.sql
+++ configdb/migrations/1758431724_create_organization_signal_expiry.up.sql
@@ -1,17 +1,3 @@
--- Copyright (C) 2025 CardinalHQ, Inc
---
--- This program is free software: you can redistribute it and/or modify
--- it under the terms of the GNU Affero General Public License as
--- published by the Free Software Foundation, version 3.
---
--- This program is distributed in the hope that it will be useful,
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
--- GNU Affero General Public License for more details.
---
--- You should have received a copy of the GNU Affero General Public License
--- along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 -- Table to configure data retention policies per organization and signal type
 -- A value of -1 means use system default, 0 means never expire, >0 means specific retention days
 CREATE TABLE organization_signal_expiry (
