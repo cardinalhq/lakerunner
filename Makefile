4666ab3433c9cb7866324d92a8102f543a5b3258
Michael Graff <mgraff@cardinalhq.io>
2026-01-26 10:56:29 -0600

    copyright year (#369)

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 CardinalHQ, Inc
+# Copyright (C) 2025-2026 CardinalHQ, Inc
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
--- docker/goreleaser-entrypoint.sh
+++ docker/goreleaser-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (C) 2025 CardinalHQ, Inc
+# Copyright (C) 2025-2026 CardinalHQ, Inc
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
